c4abb1aa3d9fa56495c53b4e9ee1f8371e785781
sonja <sonja@eleven.nl>
2020-12-04 10:15:37 +0100

    Day 4 - who does like queueing at the airport?!

---
+++ src/main/java/helpers/TestBoilerPlate.java
@@ -0,0 +1,28 @@
+package helpers;
+
+import days.Day0;
+
+public class TestBoilerPlate {
+//
+//	@Test
+//	void runA() {
+//		String input;
+//		int answer;
+//
+//		input = "";
+//		answer = 0;
+//		assertEquals(answer, Day0.runA(input), "input = " + input + " answer: " + answer);
+//
+//	}
+//
+//
+//	@Test
+//	void runB() {
+//		String input;
+//		int answer;
+//
+//		input = "";
+//		answer = 0;
+//		assertEquals(answer, Day0.runB(input), "input = " + input + " answer: " + answer);
+//	}
+}
